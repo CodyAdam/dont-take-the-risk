d07c44eaa643f1b4991e9a9d1202e23d2d7c9d5e
CodyAdam <cody.ad4@gmail.com>
2022-12-02 04:12:38 +0100

    fix(Date utils): make "ago" optional

--- utils/date-helper.ts
+++ utils/date-helper.ts
@@ -16,22 +16,30 @@ export const negativeDate = (
 
 /**
  * Date to readable string relative to now
- * ex: 2 min, 4 days, 1 month
+ * ex: 2 min ago, 4 days ago
+ * if more than 1 week ago, return the short date
  * @param date the date to convert
  */
-export const dateToRelativeString = (date: Date): string => {
+export const formatDate = (
+	date: Date,
+	ago: boolean = false
+): string => {
 	const now = new Date()
 	const diff = now.getTime() - date.getTime()
-	const diffInMinutes = Math.floor(diff / (1000 * 60))
-	const diffInHours = Math.floor(diffInMinutes / 60)
-	const diffInDays = Math.floor(diffInHours / 24)
-	const diffInMonths = Math.floor(diffInDays / 30)
-	const diffInYears = Math.floor(diffInMonths / 12)
-
-	if (diffInMinutes < 1) return 'just now'
-	if (diffInMinutes < 60) return `${diffInMinutes} min`
-	if (diffInHours < 24) return `${diffInHours} hours`
-	if (diffInDays < 30) return `${diffInDays} days`
-	if (diffInMonths < 12) return `${diffInMonths} months`
-	return `${diffInYears} years`
+	const diffDays = Math.floor(diff / (1000 * 3600 * 24))
+	const diffHours = Math.floor(diff / (1000 * 3600))
+	const diffMinutes = Math.floor(diff / (1000 * 60))
+	if (diffDays >= 7) {
+		return date.toLocaleDateString()
+	}
+	if (diffDays >= 1) {
+		return `${diffDays} days` + (ago ? ' ago' : '')
+	}
+	if (diffHours >= 1) {
+		return `${diffHours} h` + (ago ? ' ago' : '')
+	}
+	if (diffMinutes > 1) {
+		return `${diffMinutes} min` + (ago ? ' ago' : '')
+	}
+	return 'just now'
 }
