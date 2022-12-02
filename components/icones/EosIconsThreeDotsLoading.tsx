0851585887858d86a53328a964da30353fb74d75
CodyAdam <cody.ad4@gmail.com>
2022-12-02 06:49:25 +0100

    feat: add icon dots

---
+++ components/icones/EosIconsThreeDotsLoading.tsx
@@ -0,0 +1,34 @@
+import React, { SVGProps } from 'react'
+
+export function EosIconsThreeDotsLoading(props: SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			id='dots'
+			width='132px'
+			height='58px'
+			viewBox='0 0 132 58'
+			version='1.1'
+			xmlns='http://www.w3.org/2000/svg'
+			{...props}
+		>
+			<style></style>
+			<title>dots</title>
+			<desc>Created with Sketch.</desc>
+			<defs></defs>
+			<g
+				id='Page-1'
+				stroke='none'
+				strokeWidth='1'
+				fill='none'
+				fillRule='evenodd'
+			>
+				<g id='dots' fill='#A3A3A3'>
+					<circle id='dot1' cx='25' cy='30' r='13'></circle>
+					<circle id='dot2' cx='65' cy='30' r='13'></circle>
+					<circle id='dot3' cx='105' cy='30' r='13'></circle>
+				</g>
+			</g>
+		</svg>
+	)
+}
+export default EosIconsThreeDotsLoading
