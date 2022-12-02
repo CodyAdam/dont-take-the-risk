b39cc269d1f796d209c23cb2a364f1984c210a08
CodyAdam <cody.ad4@gmail.com>
2022-12-02 06:49:45 +0100

    feat: add pizza implem

--- components/messagePanel.tsx
+++ components/messagePanel.tsx
@@ -1,4 +1,5 @@
 import { Choice, Option } from '@/scenario/types'
+import { LegacyRef, useEffect, useRef } from 'react'
 import { Contact } from './contact'
 import MaterialSymbolsSendRounded from './icones/MaterialSymbolsSendRounded'
 import RiArrowLeftLine from './icones/RiArrowLeftLine'
@@ -9,7 +10,10 @@ type Props = {
 	selectedContact: string | null
 	onClose: () => void
 	onSelectChoice: (option: Option) => void
+	onSelectChoiceMarc: (option: Option) => void
 	choice: Choice | null
+	choiceMarc: Choice | null
+	isWriting?: boolean
 }
 
 export const MessagePanel = ({
@@ -17,14 +21,24 @@ export const MessagePanel = ({
 	selectedContact,
 	onClose,
 	choice,
+	choiceMarc,
 	onSelectChoice,
+	isWriting,
+	onSelectChoiceMarc,
 }: Props) => {
+	const scrollDiv = useRef<null | HTMLDivElement>(null)
+	useEffect(() => {
+		//Scroll to bottom
+		if (scrollDiv.current)
+			scrollDiv.current.scrollTop = scrollDiv.current.scrollHeight
+	})
+
 	if (!selectedContact) return null
 	const contact = contacts.find((c) => c.name === selectedContact)
 	if (!contact) return null
 
 	return (
-		<div className=' mx-auto flex h-full w-full max-w-2xl flex-col '>
+		<div className=' mx-auto flex h-full w-full max-w-2xl flex-col'>
 			<div className='flex h-20 w-full items-center gap-2 p-5'>
 				<button
 					onClick={() => onClose()}
@@ -34,7 +48,10 @@ export const MessagePanel = ({
 				</button>
 				<h1 className='text-xl font-medium'>{contact.name}</h1>
 			</div>
-			<div className='flex grow flex-col gap-5 overflow-y-auto p-5'>
+			<div
+				ref={scrollDiv}
+				className='flex grow flex-col gap-5 overflow-y-auto p-5'
+			>
 				{contact.messages.map((message) => (
 					<Message
 						key={message.date.getTime()}
@@ -42,15 +59,48 @@ export const MessagePanel = ({
 						contact={contact}
 					/>
 				))}
+				{isWriting && (
+					<Message
+						contact={contact}
+						isWriting
+						message={{
+							content: '',
+							date: new Date(),
+							fromMe: false,
+							seen: false,
+						}}
+					/>
+				)}
 			</div>
 			<div className='flex flex-col gap-5 p-5'>
-				<div className='flex w-full flex-wrap gap-4'>
-					{choice && (
+				<div className='flex w-full max-w-2xl flex-wrap gap-4'>
+					{selectedContact == 'Sophie' && choice && (
 						<>
 							{choice.options.map((option) => (
 								<button
 									key={option.text}
-									onClick={() => onSelectChoice(option)}
+									onClick={() => {
+										if (contact.name == 'Livreur de pizza')
+											onSelectChoiceMarc(option)
+										else onSelectChoice(option)
+									}}
+									className='btn-outline btn rounded-xl border-2'
+								>
+									{option.text}
+								</button>
+							))}
+						</>
+					)}
+					{selectedContact == 'Livreur de pizza' && choiceMarc && (
+						<>
+							{choiceMarc.options.map((option) => (
+								<button
+									key={option.text}
+									onClick={() => {
+										if (contact.name == 'Livreur de pizza')
+											onSelectChoiceMarc(option)
+										else onSelectChoice(option)
+									}}
 									className='btn-outline btn rounded-xl border-2'
 								>
 									{option.text}
